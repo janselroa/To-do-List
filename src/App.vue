6d0b0cc4d92d4477d96b80e433081f4a624bbbf6
jansel <janselroa2424@gmail.com>
2021-11-26 19:03:03 -0400

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,174 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="settings">
+
+        <span @click="settingsActive" class="material-icons settings-icon">
+            settings
+        </span>
+
+      <div v-if="settingsActivated" class="theme-segttins">
+        <div></div>
+        <img src="@/assets/ocean.jpg" alt="ocean">
+        <img src="@/assets/nature.jpg" alt="nature">
+        <img src="@/assets/city.jpg" alt="city">
+      </div>
+    </div>
+<div class="card-input">
+    <input type="text" v-model="newTask.title" placeholder="Title">
+    <input type="text" v-model="newTask.description" placeholder="Description">
+    <button @click="createTasks">Create Task</button>
+</div>
+<div class="tasks">
+    <tarea v-for="(task,$index) in listTasks" :key="$index" :title="task.title" :description="task.description" :complet="task.complet"
+      @complet_tasks="complet_tasks($index)"
+    ></tarea>
+    </div>
+    
+    <footer class="footer">Created By Jansel Roa 2021 &copy;<a href="http://github.com/janselroa" target="_blank">Janselroa</a> </footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import tarea from "@/components/tarea.vue";
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    tarea,
+  },
+  created(){
+    if(localStorage.getItem('listTasks')){
+      this.listTasks=JSON.parse(localStorage.getItem('listTasks'));
+    }
+  },
+  data(){
+    return {
+      listTasks:[],
+      newTask:{},
+      settingsActivated:false,
+    }
+  },
+  methods: {
+    createTasks(){
+      if(this.newTask.title!=null){
+          this.newTask.complet=false;
+          this.listTasks.unshift(this.newTask);
+          localStorage.setItem("listTasks", JSON.stringify(this.listTasks));
+          this.newTask={};
+      }
+    },
+    complet_tasks(index){
+      if(this.listTasks[index].complet) this.listTasks[index].complet=false; 
+      else this.listTasks[index].complet=true;
+       localStorage.setItem("listTasks", JSON.stringify(this.listTasks));
+    },
+    settingsActive(){
+      console.log("asd")
+      this.settingsActivated=!this.settingsActivated;
+      console.log(this.settingsActived)
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+<style lang="scss">
+@font-face {
+  font-family: 'Material Icons';
+  font-style: normal;
+  font-weight: 400;
+  src: url(https://example.com/MaterialIcons-Regular.eot); /* For IE6-8 */
+  src: local('Material Icons'),
+    local('MaterialIcons-Regular'),
+    url(https://example.com/MaterialIcons-Regular.woff2) format('woff2'),
+    url(https://example.com/MaterialIcons-Regular.woff) format('woff'),
+    url(https://example.com/MaterialIcons-Regular.ttf) format('truetype');
+}
+
+body{
+  height:100vh;
+  font-family:'Roboto';
+  background:no-repeat url("assets/ocean.jpg");
+  background-size: cover;
+}
+#app{
+  color:#fff;
+  height:100vh;
+  display: flex;
+  flex-direction: column;
+}
+.card-input{
+  width:400px;
+  margin: 100px auto;
+  input{
+    display:block;
+    width:100%;
+    background-color: hsla(0, 0%, 20%, 0.856);
+    color:#fff;
+    margin: 10px 0;
+  }
+  input,button{
+    border: none;
+    outline: none;
+    padding:12px;
+  }
+  button{
+    display: block;
+    width:100px;
+    margin: auto;
+    color:#fff;
+    background-color: #222;
+  }
+}
+.tasks{
+  width:100%;
+  margin-top:100px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+.settings{
+  font-size:2.3rem;
+  width:400px;
+  position:absolute;
+  top: 50px;
+  right: 20px;
+}
+.settings span{
+  font-size:2.3rem;
+  position: absolute;
+  top:-30px;
+  right: 20px;
+}
+.theme-segttins{
+  width:400px;
+  height: 200px;
+  display: flex;
+  flex-wrap: wrap;
+  position: absolute;
+  top:20px;
+  background-color:#333;
+  overflow-y: auto;
+  img{
+    width:150px;
+    height:100px;
+    object-fit: cover;
+    margin: 5px;
+  }
+  div{
+    width:150px;
+    height: 100px;
+    margin: 5px;
+    background-color:#222;
+
+  }
+  img:hover,div:hover{
+    outline: 1px solid #ddd;
+  }
+}
+.footer{
+  margin-top: auto;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+.footer a{
+  color:inherit;
 }
 </style>
---
+++ src/components/tarea.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="task" :class="{'complet':complet}">
+        <div @click="complet_tasks" class="complet-check">
+            <i class="material-icons">done</i>
+        </div>
+        <div class="tasks__content">
+            <h2>{{title}}</h2>
+            <p>{{description}}</p>
+        </div>
+        
+    </div>
+</template>
+<script>
+export default {
+    props:['title','description','complet'],
+    methods:{
+        complet_tasks(){
+            this.complet=!this.complet;
+            this.$emit('complet_tasks')
+        }
+    },
+}
+</script>
+<style scoped>
+.task{
+    width:350px;
+    padding:10px;
+    margin:20px;
+    background-color: hsla(0, 0%, 20%, 0.807);
+    color:#fff;
+    border-radius: 10px;
+    display: flex;
+}
+.task h2{
+    margin:0;
+}
+.tasks__content{
+    width:300px;
+    overflow-y: auto;
+}
+.complet{
+    opacity: 0.8;
+}
+.complet .complet-check{
+    background-color:rgb(163, 229, 255);
+    color: hsla(0, 0%, 20%);
+}
+.complet-check{
+    width:30px;
+    height:30px;
+    margin-right: 20px;
+    display: flex;
+    justify-content:center;
+    align-items:center;
+    color:transparent;
+    border: 1px solid #aaa;
+    font-size:1.3rem;
+    border-radius: 20px;
+    cursor: pointer;
+}
+.task:hover{
+    background-color: hsla(0, 0%, 20%, 0.860);
+}
+span{
+    color:inherit;
+}
+</style>
